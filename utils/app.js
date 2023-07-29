8692621da835c7d4e97163fc4d01e58b75f7337a
NaraeJang <j.naaaarae@gmail.com>
2023-07-29 00:01:44 -0400

    completed the project

--- utils/app.js
+++ utils/app.js
@@ -1,19 +1,17 @@
 import get from "./getElement.js";
 import fetchUser from "./fetchUser.js";
+import displayUser from "./displayUser.js";
 
 const URL = "https://randomuser.me/api/";
 
-const img = get(".user-img");
-const title = get(".user-title");
-const value = get(".user-value");
 const btn = get(".btn");
-const btns = [...document.querySelectorAll(".icon")];
 
 const showUser = async () => {
   // fetch user
   const person = await fetchUser(URL);
 
   // display user
+  displayUser(person);
 };
 
 window.addEventListener("DOMContentLoaded", showUser);
